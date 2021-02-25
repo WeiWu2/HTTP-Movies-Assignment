212cfe10a113123f2b633e4d7c1d1799bcf742a7
Bansa Laura <weidiwu207@gmail.com>
2021-02-25 15:51:17 -0500

    met mvp

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -1,23 +1,48 @@
-import React, {useState} from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
 
 export default function AddMovie() {
+    const { push } = useHistory();
+    const [starList, setStarList] = useState({starOne:'', starTwo:'', starThree:''})
     const [movie, setMovie] = useState({
         id:'',
         title:'',
         director:'',
         metascore:'',
-        stars:[]
-
+        stars:['']
     })
-    const handleAdd = () => {}
-    const handleChanges = (e) => {
-           setMovie({
+    useEffect(() => {
+        setMovie({
             ...movie,
-            [e.target.name]: e.target.value
+            stars:[starList.starOne, starList.starTwo, starList.starThree]
+        })
+    },[starList])
+    const handleAdd = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/movies', movie)
+        .then((res) => {
+        push('/')
+        })
+        .catch((err) => {
+            console.log(err)
         })
     }
-    const handleKeyDown = (e) => {
+    const handleChanges = (e) => {
+        
+            setMovie({
+            ...movie,
+            [e.target.name]: e.target.value
+            
+        })
        
+    
+    }
+    const handleStar = (e) => {
+        setStarList({
+            ...starList,
+            [e.target.name] : e.target.value
+        })
     }
     return (
         <div>
@@ -36,7 +61,15 @@ export default function AddMovie() {
                 </div>
                 <div>
                 <label>Stars: </label>
-                <input name='stars' value={movie.stars} type='text' onKeyDown={handleKeyDown}></input>
+                <input name='starOne' value={starList.starOne} type='text' onChange={handleStar} ></input>
+                </div>
+                <div>
+                <label>Stars: </label>
+                <input name='starTwo' value={starList.starTwo} type='text' onChange={handleStar} ></input>
+                </div>
+                <div>
+                <label>Stars: </label>
+                <input name='starThree' value={starList.starThree} type='text' onChange={handleStar} ></input>
                 </div>
                 <button>Add</button>
             </form>
